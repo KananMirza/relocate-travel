7624789719bb5e0d626fe5f93d4184589e851637
Kanan Mirzayev <58333179+KananMirza@users.noreply.github.com>
2021-09-11 16:20:54 +0400

    add material design cards

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+
+    $(window).scroll(()=>{
+      if($(window).scrollTop()>100){
+      $('#up').slideDown('slow')
+      }else{
+          $('#up').slideUp('slow')
+      }
+  })
+
+  $('#up').click(()=>{
+      $('html,body').animate({scrollTop:0},1000)
+      return false;
+  })
+
+  $('.bottom').click(()=>{
+    $('html,body').animate({scrollTop:835},1000)
+    return false;
+})
+$('.tabs').tabs();
+
   });
